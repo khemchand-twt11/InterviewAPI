eb5d1a8b44229668086ed1a46faf74383961a42c
khemchand-twt11 <gauravsharma6290852476@gmail.com>
2023-06-23 23:06:16 +0530

    frontend

---
+++ client/src/components/UserDashboardComponent.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function UserDashboardComponent() {
+  const [selectedValue, setSelectedValue] = useState("javascript");
+
+  useEffect(() => {
+    console.log(selectedValue);
+  }, [selectedValue]);
+
+  const handleSelectChange = (e) => {
+    setSelectedValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <select
+        id="select-option"
+        value={selectedValue}
+        onChange={handleSelectChange}
+      >
+        <option value="javascript">JavaScript</option>
+        <option value="react">React</option>
+        <option value="node">Node</option>
+        <option value="html">HTML</option>
+        <option value="css">CSS</option>
+        <option value="java">Java</option>
+      </select>
+      <button>
+        <Link to={`/interview/${selectedValue}`}>Take Interveiw</Link>
+      </button>
+    </div>
+  );
+}
---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,6 @@
+import SignupComponent from "../components/SignupComponent";
+const Signup = () => {
+  return <SignupComponent />;
+};
+
+export default Signup;
