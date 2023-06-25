60357823cecd5881e4431d03ac6b5394881f65b8
hariomfw21 <darkphinixgamer@gmail.com>
2023-06-25 17:39:15 +0530

    structuring the folder

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,11 +1,20 @@
-const auth= (req,res,next)=>{
-    const token = req.headers.authorization;;
-    if(!token){
-        res.status(403).send({message:'you must be logged in'});
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(403).send({ message: 'you must be logged in' }); // return here
     }
-    next();
+
+    jwt.verify(token,process.env.SECRET_KEY, (err,data)=>{
+        if(err){
+            return res.status(403).send({ message: 'you must be logged in again' }); // return here
+        }else{
+            next();
+        }
+    })
 }
 
-module.exports={
+module.exports = {
     auth
 }
